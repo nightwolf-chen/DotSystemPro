9ea1f7b04a50a1314acb9a82d2e487318f5c879f
nirvawolf <nirvawolf@qq.com>
2013-12-20 19:02:24 +0800

    add Magnager instance management inside UpdateCenter.Also implement basic initailize method for UpdateCenter

--- DotSystemPro/dotsys/BCAppInfoUpdateCenter.m
+++ DotSystemPro/dotsys/BCAppInfoUpdateCenter.m
@@ -7,7 +7,37 @@
 //
 
 #import "BCAppInfoUpdateCenter.h"
+#import "BCAppInfoManager.h"
+#include "Notifications.h"
 
 @implementation BCAppInfoUpdateCenter
+
++ (id)sharedInstance
+{
+    static BCAppInfoUpdateCenter *_sharedInstance;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _sharedInstance = [[BCAppInfoUpdateCenter alloc] _init];
+    });
+    return _sharedInstance;
+}
+
+- (id)_init
+{
+    self = [super init];
+    if (self) {
+        _appInfoManger = [[BCAppInfoManager instance] retain];
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(onLoginSuccess)
+                                                     name:LoginSuccessNotification object:nil];
+    }
+    return self;
+}
+
+- (void)onLoginSuccess
+{
+    if (_appInfoManger) { [_appInfoManger release]; }
+    _appInfoManger = [[BCAppInfoManager instance] retain];;
+}
 
 @end
